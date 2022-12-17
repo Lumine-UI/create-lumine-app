449b6af71d190d8daf3c517a687a6cba8e0301ae
Siddhesh Kothadi <realsiddheshko@gmail.com>
2022-12-17 17:19:36 +0530

    Uses curl for updating App.tsx

--- bin/setup-lumine.sh
+++ bin/setup-lumine.sh
@@ -3,17 +3,29 @@
 # Get the project name from the first command line argument
 project_name=$1
 
+echo ""
 echo "Creating new Expo app with name '$project_name'..."
 
 npx create-expo-app -t expo-template-blank-typescript $project_name
 
+echo ""
 echo "Installing lumine-ui dependency..."
 
 cd $project_name
 npm install lumine-ui
 
+echo ""
 echo "Updating App.tsx to use lumine-ui..."
 
-cp ../App.tsx .
+curl https://raw.githubusercontent.com/Lumine-UI/create-lumine-app/main/App.tsx -o App.tsx
 
+echo ""
 echo "Done!"
+
+echo ""
+echo "To run your app, run the following commands:"
+
+echo ""
+echo "cd $project_name"
+echo "npm start"
+
